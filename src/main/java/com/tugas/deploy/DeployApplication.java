a1e30d2aeb4f1401a61e6d377861f65e2490b118
Azkalyasin <yasinadzkiaazkal@gmail.com>
2026-04-13 13:54:06 +0700

    penambhan deplotyaplicatin

--- src/main/java/com/tugas/deploy/DeployApplication.java
+++ src/main/java/com/tugas/deploy/DeployApplication.java
@@ -1,4 +1,4 @@
-package com.example.deploy;
+package com.tugas.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tugas/deploy/DeployApplicationTests.java
+++ src/test/java/com/tugas/deploy/DeployApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.deploy;
+package com.tugas.deploy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
